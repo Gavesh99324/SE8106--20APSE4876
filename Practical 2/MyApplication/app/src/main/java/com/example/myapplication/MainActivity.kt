45f4664fb62a1efccbad136ccf2edc7f6532aab7
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2026-03-20 10:24:15 +0530

    practical 2

---
+++ Practical 2/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyApplicationTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(modifier = Modifier.padding(innerPadding)) {
+                        Greeting(name = "Android")
+                        Hello()
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier,
+        fontSize = 28.sp,
+        fontWeight = FontWeight.Bold
+    )
+}
+
+@Composable
+fun Hello() {
+    Text(
+        text = "Hello Students",
+        color = Color.Blue,
+        fontSize = 20.sp
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApplicationTheme {
+        Column {
+            Greeting("Android")
+            Hello()
+        }
+    }
+}
